e7ff0728af279aa93331346827cfac4888c1a0db
Dinuka Sandakalum <dinukasandakalum125@gmail.com>
2022-09-03 23:46:37 +0530

    created

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr= {-10,25,50,60,-40,80,-22};
+
+        System.out.println(" max = "+ new Max().max(arr));
+        System.out.println(" min= " + new Min().min(arr));
+
+    }
+}
---
+++ src/Max.java
@@ -0,0 +1,13 @@
+public class Max {
+
+    public int max (int[] a){
+        int max = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (a[i]>max){
+                max = a[i];
+            }
+        }
+        return max;
+    }
+
+}
